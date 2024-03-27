13965c3ca42df6fe30dfae61719d7274c75636fa
FC-LinuxMachine <i_cherkasov@fermer-centr.shop>
2024-03-27 21:08:31 +0700

    step-6: add taskStatuses to InitialDataInitialization

--- src/main/java/hexlet/code/app/component/InitialDataInitialization.java
+++ src/main/java/hexlet/code/app/component/InitialDataInitialization.java
@@ -1,6 +1,8 @@
 package hexlet.code.app.component;
 
+import hexlet.code.app.model.TaskStatus;
 import hexlet.code.app.model.User;
+import hexlet.code.app.repository.TaskStatusRepository;
 import hexlet.code.app.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @AllArgsConstructor
@@ -17,19 +21,32 @@ public class InitialDataInitialization implements ApplicationRunner {
     private final UserRepository userRepository;
 
     @Autowired
+    private final TaskStatusRepository taskStatusRepository;
+
+    @Autowired
     private final PasswordEncoder passwordEncoder;
 
-    private final String emailService = "hexlet@example.com";
-    private final String passService = "qwerty";
+    private final String defaultEmail = "hexlet@example.com";
+
+    private final String defaultPass = "qwerty";
+
+    private final List<String> defaultTaskStatuses = List.of(
+            "draft", "to_review", "to_be_fixed", "to_publish", "published");
 
     @Override
     public void run(ApplicationArguments args) {
-        userRepository.findByEmail(emailService).orElseGet(() -> {
-            var user = new User();
-            user.setEmail(emailService);
-            user.setPasswordDigest(passwordEncoder.encode(passService));
+        userRepository.findByEmail(defaultEmail).orElseGet(() -> {
+            var user = new User()
+                    .setEmail(defaultEmail)
+                    .setPasswordDigest(passwordEncoder.encode(defaultPass));
             return userRepository.save(user);
         });
+
+        defaultTaskStatuses.stream().map(s -> taskStatusRepository.findBySlug(s)
+                .orElse(new TaskStatus()
+                        .setName(s.substring(0, 1).toUpperCase() + s.substring(1))
+                        .setSlug(s)
+                )).forEach(taskStatusRepository::save);
 
     }
 
