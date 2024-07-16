7a9e0b14dd1a9f19a38d1a91dfe3d2174985c15c
FC-LinuxMachine <i_cherkasov@fermer-centr.shop>
2024-07-16 22:34:07 +0700

    step-8: add User service & refactor User controller

--- src/main/java/hexlet/code/app/controller/UsersController.java
+++ src/main/java/hexlet/code/app/controller/UsersController.java
@@ -3,14 +3,14 @@ package hexlet.code.app.controller;
 import hexlet.code.app.dto.user.UserCreateDTO;
 import hexlet.code.app.dto.user.UserDTO;
 import hexlet.code.app.dto.user.UserUpdateDTO;
-import hexlet.code.app.exception.ResourceNotFoundException;
-import hexlet.code.app.mapper.UserMapper;
-import hexlet.code.app.repository.UserRepository;
+import hexlet.code.app.service.UserService;
+import hexlet.code.app.util.UserUtils;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,67 +24,52 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("${base-url}${users-url}")
+@RequestMapping("/api/users")
 @AllArgsConstructor
 public class UsersController {
 
     @Autowired
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     @Autowired
-    private final UserMapper userMapper;
-
-    @Autowired
-    private final PasswordEncoder passwordEncoder;
+    private final UserUtils userUtils;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<UserDTO> list() {
-        var users = userRepository.findAll();
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        List<UserDTO> users = userService.getAllUsers();
 
-        return users.stream().map(userMapper::map).toList();
-
+        return ResponseEntity
+                .ok()
+                .header("X-Total-Count", String.valueOf(users.size()))
+                .body(users);
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public UserDTO getUser(@PathVariable final long id) {
-        var user = userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format("User with id %s not found", id)));
-
-        return userMapper.map(user);
-
+        return userService.getUserById(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public UserDTO createUser(@Valid @RequestBody final UserCreateDTO userBody) {
-        var user = userMapper.map(userBody);
-        var encodePassword = passwordEncoder.encode(user.getPassword());
-        user.setPasswordDigest(encodePassword);
-        userRepository.save(user);
-
-        return userMapper.map(user);
-
+        return userService.createUser(userBody);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("@userUtils.isUser(#id")
     public UserDTO updateUser(@Valid @RequestBody final UserUpdateDTO userBody,
                               @PathVariable final long id) {
-        var user = userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format("User with id %s not found", id)));
-        userMapper.update(userBody, user);
-        userRepository.save(user);
-
-        return userMapper.map(user);
-
+        return userService.updateUser(userBody, id);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("@userUtils.isUser(#id")
     public void deleteUser(@PathVariable final long id) {
-        userRepository.deleteById(id);
+        userService.deleteUser(id);
     }
 
 }
---
+++ src/main/java/hexlet/code/app/service/UserService.java
@@ -0,0 +1,61 @@
+package hexlet.code.app.service;
+
+import hexlet.code.app.dto.user.UserCreateDTO;
+import hexlet.code.app.dto.user.UserDTO;
+import hexlet.code.app.dto.user.UserUpdateDTO;
+import hexlet.code.app.exception.ResourceNotFoundException;
+import hexlet.code.app.mapper.UserMapper;
+import hexlet.code.app.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+    @Autowired
+    private final UserMapper userMapper;
+
+    public List<UserDTO> getAllUsers() {
+        var users = userRepository.findAll();
+
+        return users.stream().map(userMapper::map).toList();
+
+    }
+
+    public UserDTO getUserById(final long id) {
+        var user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("User with id %s not found", id)));
+
+        return userMapper.map(user);
+
+    }
+
+    public UserDTO createUser(final UserCreateDTO userCreateDTO) {
+        var user = userMapper.map(userCreateDTO);
+        userRepository.save(user);
+
+        return userMapper.map(user);
+
+    }
+
+    public UserDTO updateUser(final UserUpdateDTO userUpdateDTO, final long id) {
+        var user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("User with id %s not found", id)));
+        userMapper.update(userUpdateDTO, user);
+        userRepository.save(user);
+
+        return userMapper.map(user);
+
+    }
+
+    public void deleteUser(final long id) {
+        userRepository.deleteById(id);
+    }
+
+}
